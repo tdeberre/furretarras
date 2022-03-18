fdff6e591a42d87924340a2ae00a5c8f15a111dd
NicolasDeFalco <nicolasdefalco.9@gmail.com>
2022-03-18 10:21:54 +0100

    First commit to model

--- bin/controleur.dart
+++ bin/controleur.dart
@@ -1,1 +1,12 @@
-main(List<String> args) {}
+import 'dart:io';
+import 'package:mysql1/mysql1.dart';
+
+main(List<String> args) {
+  ConnectionSettings settings = ConnectionSettings(
+    host: 'localhost',
+    port: 3306,
+    user: 'user',
+    password: 'password',
+    db: 'db',
+  );
+}
