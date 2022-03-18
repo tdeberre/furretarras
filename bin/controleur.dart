76aea9548d9c35e376e877362ab285137bc56256
Theo <deberretheo@gmail.com>
2022-03-18 10:11:31 +0100

    1er commit vue

--- bin/controleur.dart
+++ bin/controleur.dart
@@ -1,1 +1,20 @@
-main(List<String> args) {}
+import 'vue.dart';
+import 'modele.dart';
+
+main(List<String> args) {
+  int choix = 0;
+  while (choix != 3) {
+    choix = Vue.demanderaction();
+    switch (choix) {
+      //si rechercher
+      case 1:
+        String critere = Vue.demandercritere();
+        String valeur = Vue.demandervaleur();
+        DB.chercherproduit(critere, valeur);
+        break;
+      //si modifier
+      case 2:
+        break;
+    }
+  }
+}
